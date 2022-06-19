c72713e21e4c28112bffb0b79e6851ba2598d075
casdjani <nazira.andjani@gmail.com>
2022-06-19 21:30:11 +0930

    challenge 09

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,10 +1,18 @@
 // Terima props lalu tampilkan dalam Contact component
 // Kalian bisa membuat CSS sendiri di src/components/Contact.css
+import './Contact.css'
 import React from 'react';
 
-const Contact = () => {
+const Contact = ({data}) => {
     return (
-        <></>
+        <div className='ProfileWrapper'>
+            <img className='ImgProfile' src={data.photo} />
+            <div>
+                <h2>{data.name}</h2>
+                <p>{data.phone}</p>
+                <p>{data.email}</p>
+            </div>
+        </div>
     )
 }
 
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,14 @@
 // Kalian bisa menambahkan CSS di src/components/Header.css
+import './Header.css'
 import React from 'react';
 
 const Header = () => {
     return (
-        <></>
+        <>
+            <h1>Call a friend</h1>
+            <p>your friendly contact app</p>
+            <hr className="Liner" />
+        </>
     )
 }
 
